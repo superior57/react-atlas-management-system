3afd7881093e26427c5b38a4beb7c8034e6a6f6d
talentedexpert0057 <petpanda0057@gmail.com>
2020-10-29 01:39:17 +1000

    frontend done for search crew page

--- src/app/main/crew/search/BodyContent.js
+++ src/app/main/crew/search/BodyContent.js
@@ -1,4 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 import React, {
 	useState
 } from 'react';
@@ -12,6 +21,7 @@ import {
 	MenuItem
 } from '@material-ui/core';
 import clsx from "clsx";
+import HeaderContent from './HeaderContent';
 
 const useStyles = makeStyles(theme => ({
 	layoutRoot: {},
@@ -22,7 +32,38 @@ const useStyles = makeStyles(theme => ({
 	formControl: {
 		minWidth: 120,
 	},
+	table: {
+		minWidth: 650,
+	},
+	marginLeft: {
+		marginLeft: '-4px'
+	}
 }));
+  
+function createData( id, m, lastname, firstname, father, mother, age, licrank, nationality, birthdate, avfrom, avto, lastrank, vessel, status ) {
+	return { id, m, lastname, firstname, father, mother, age, licrank, nationality, birthdate, avfrom, avto, lastrank, vessel, status };
+}
+  
+const rows = [
+	createData('100001', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100002', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100003', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100004', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100005', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100006', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100007', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100008', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100009', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100010', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100011', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100012', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100013', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100014', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100015', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100016', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+	createData('100017', "ETNK", "ABRAMENKO", "VOLODYMYR", "STANISLAV", "n/a", "68", "MASTER", "Ukraine", "25/08/52", "", "", "MASTER", "MARU", "OFB"),
+	createData('100018', "ETNK", "LYKOURIS", "ANTONIOS", "", "", "66", "MASTER", "Greek", "25/09/54", "", "", "MASTER", "URANIA", "OFB"),
+];
 
 function BodyContent(props) {
 	const [state, setState] = useState({
@@ -39,7 +80,52 @@ function BodyContent(props) {
 
 	const classes = useStyles(props);
 	return <>
-	 	<h3>Body ...</h3>
+	 	<HeaderContent />
+
+		<TableContainer component={Paper} className={classes.marginLeft}>
+			<Table className={classes.table} size="small" aria-label="a dense table">
+				<TableHead>
+					<TableRow>
+						<TableCell>ID</TableCell>
+						<TableCell>M</TableCell>
+						<TableCell>Lastname</TableCell>
+						<TableCell>Firstname</TableCell>
+						<TableCell>Father</TableCell>
+						<TableCell>Mother</TableCell>
+						<TableCell>Age</TableCell>
+						<TableCell>Lic Rank</TableCell>
+						<TableCell>Nationality</TableCell>
+						<TableCell>Birthdate</TableCell>
+						<TableCell>Av.From</TableCell>
+						<TableCell>Av.to</TableCell>
+						<TableCell>Last Rank</TableCell>
+						<TableCell>Vessel</TableCell>
+						<TableCell>Status</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
+						<TableRow key={row.name}>
+							<TableCell>{row.id}</TableCell>
+							<TableCell>{row.m}</TableCell>
+							<TableCell>{row.lastname}</TableCell>
+							<TableCell>{row.firstname}</TableCell>
+							<TableCell>{row.father}</TableCell>
+							<TableCell>{row.mother}</TableCell>
+							<TableCell>{row.age}</TableCell>
+							<TableCell>{row.licrank}</TableCell>
+							<TableCell>{row.nationality}</TableCell>
+							<TableCell>{row.birthdate}</TableCell>
+							<TableCell>{row.avfrom}</TableCell>
+							<TableCell>{row.avto}</TableCell>
+							<TableCell>{row.lastrank}</TableCell>
+							<TableCell>{row.vessel}</TableCell>
+							<TableCell>{row.status}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	</>;
 }
 
--- src/app/main/crew/search/HeaderContent.js
+++ src/app/main/crew/search/HeaderContent.js
@@ -1,4 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuList from '@material-ui/core/MenuList';
+import SplitButton from './SplitButton'
+
+
 import React, {
 	useState
 } from 'react';
@@ -12,6 +24,7 @@ import {
 	MenuItem
 } from '@material-ui/core';
 import clsx from "clsx";
+import { ClassSharp } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
 	layoutRoot: {},
@@ -22,7 +35,25 @@ const useStyles = makeStyles(theme => ({
 	formControl: {
 		minWidth: 120,
 	},
+	spacingBottom: {
+		marginBottom: "1rem"
+	},
+	spacingBottomSmall: {
+		marginBottom: '0.5rem'
+	},
+	select: {
+		height:40
+	},
+	noPadding: {
+		padding:0,
+		minWidth:'15px'
+	},
+	noTransform: {
+		textTransform:"none"
+	}
 }));
+
+
 
 function HeaderContent(props) {
 	const [state, setState] = useState({
@@ -34,12 +65,252 @@ function HeaderContent(props) {
 		religion: "",
 		marital_status: "",
 		manning_agent: "",
-		only_for_manager: ""
+		only_for_manager: "",
+		has_appraisal_as:"",
+		prserv_with:"",
+		nation:"",
+		onlyformanaget:"",
+		overalevaluation:"",
+		onb_ofb_with:""
 	});
 
 	const classes = useStyles(props);
 	return <>
-	 	<h3>Header ... </h3>
+	 	<Grid container spacing={2} className={clsx(classes.spacingBottom)}>
+			<Grid item xs={12} md={9}>
+				<Grid container spacing="1" className="px-10 py-5 bg-white border border-gray-400">
+					<Grid item xs={12} md={6}>
+						<TextField							
+							label="Name"
+							defaultValue=""
+							variant="outlined"		
+							className={clsx(classes.textField, classes.spacingBottom)} 
+							size='small'
+						/>
+						<Grid container spacing='1'>
+							<Grid item xs={12} md={6} >
+								<div>
+									<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5")} size='small'>
+										<InputLabel id="rank-label">Rank</InputLabel>
+										<Select
+											labelId="rank-label"
+											value={state.rank}
+											onChange={(e) => {
+												setState({
+													...state,
+													rank: e.target.value
+												});
+											}}
+											label="Rank"
+											
+										>
+										<MenuItem value="">
+											<em>None</em>
+										</MenuItem>
+										<MenuItem value={1}>Rank 1</MenuItem>
+										<MenuItem value={2}>Rank 2</MenuItem>
+										<MenuItem value={3}>Rank 3</MenuItem>
+										</Select>
+									</FormControl>
+								</div>
+							</Grid>
+							<Grid item xs={12} md={6}>
+								<div>
+									<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5",classes.spacingBottomSmall)} size='small'>
+										<InputLabel id="nation">Nation</InputLabel>
+										<Select
+											labelId="nation"
+											value={state.nation}
+											onChange={(e) => {
+												setState({
+													...state,
+													nation: e.target.value
+												});
+											}}
+											label="Nation"
+										>
+										<MenuItem value="">
+											<em>None</em>
+										</MenuItem>
+										<MenuItem value={1}>Rank 1</MenuItem>
+										<MenuItem value={2}>Rank 2</MenuItem>
+										<MenuItem value={3}>Rank 3</MenuItem>
+										</Select>
+									</FormControl>
+								</div>
+							</Grid>
+						</Grid>
+						<Grid container spacing='1'>
+							<Grid item xs={12} md={6}>
+								
+							</Grid>
+							<Grid item xs={12} md={6}>
+								<div>
+									<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5")} size='small'>
+										<InputLabel id="onlyformanager">Only for Manager</InputLabel>
+										<Select
+											labelId="onlyformanager"
+											value={state.onlyformanager}
+											onChange={(e) => {
+												setState({
+													...state,
+													onlyformanager: e.target.value
+												});
+											}}
+											label="Only for Manager"
+											
+										>
+										<MenuItem value="">
+											<em>None</em>
+										</MenuItem>
+										<MenuItem value={1}>Rank 1</MenuItem>
+										<MenuItem value={2}>Rank 2</MenuItem>
+										<MenuItem value={3}>Rank 3</MenuItem>
+										</Select>
+									</FormControl>
+								</div>
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item xs={12} md={3}>
+						<div className="w-full">
+							<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5",classes.spacingBottom)} size='small'>
+								<InputLabel id="currentstatus">Current Status</InputLabel>
+								<Select
+									labelId="currentstatus"
+									value={state.currentstatus}
+									onChange={(e) => {
+										setState({
+											...state,
+											currentstatus: e.target.value
+										});
+									}}
+									label="Current Status"
+									
+								>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={1}>Rank 1</MenuItem>
+								<MenuItem value={2}>Rank 2</MenuItem>
+								<MenuItem value={3}>Rank 3</MenuItem>
+								</Select>
+							</FormControl>
+						</div>
+						<TextField							
+							label="Name"
+							defaultValue=""
+							variant="outlined"		
+							className={clsx(classes.textField, classes.spacingBottom)} 
+							size='small'
+						/>
+						<div className="w-full">
+							<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5",classes.spacingBottom)} size='small'>
+								<InputLabel id="overalevaluation">Overal Evaluation</InputLabel>
+								<Select
+									labelId="overalevaluation"
+									value={state.overalevaluation}
+									onChange={(e) => {
+										setState({
+											...state,
+											overalevaluation: e.target.value
+										});
+									}}
+									label="Overal Evaluation"
+									
+								>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={1}>Rank 1</MenuItem>
+								<MenuItem value={2}>Rank 2</MenuItem>
+								<MenuItem value={3}>Rank 3</MenuItem>
+								</Select>
+							</FormControl>
+						</div>
+					</Grid>
+					<Grid item xs={12} md={3}>
+						<div className="w-full">
+							<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5", classes.spacingBottom)} size='small'>
+								<InputLabel id="onb_ofb_with">ONB/OFB with</InputLabel>
+								<Select
+									labelId="onb_ofb_with"
+									value={state.onb_ofb_with}
+									onChange={(e) => {
+										setState({
+											...state,
+											onb_ofb_with: e.target.value
+										});
+									}}
+									label="ONB/OFB with"
+									
+								>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={1}>Rank 1</MenuItem>
+								<MenuItem value={2}>Rank 2</MenuItem>
+								<MenuItem value={3}>Rank 3</MenuItem>
+								</Select>
+							</FormControl>
+						</div>
+						<div className="w-full">
+							<FormControl required variant="outlined" className={clsx(classes.formControl, "w-full mr-5", classes.spacingBottom)} size='small'>
+								<InputLabel id="prserv_with">Prserv with</InputLabel>
+								<Select
+									labelId="prserv_with"
+									value={state.prserv_with}
+									onChange={(e) => {
+										setState({
+											...state,
+											prserv_with: e.target.value
+										});
+									}}
+									label="Prserv with"
+									
+								>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={1}>Rank 1</MenuItem>
+								<MenuItem value={2}>Rank 2</MenuItem>
+								<MenuItem value={3}>Rank 3</MenuItem>
+								</Select>
+							</FormControl>
+						</div>
+						<div className="w-full">
+							<FormControl variant="outlined" className={clsx(classes.formControl, "w-full mr-5")} size='small'>
+								<InputLabel id="has_appraisal_as">Has Appraisal as</InputLabel>
+								<Select
+									labelId="has_appraisal_as"
+									value={state.has_appraisal_as}
+									onChange={(e) => {
+										setState({
+											...state,
+											has_appraisal_as: e.target.value
+										});
+									}}
+									label="Has Appraisal as"
+									
+								>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={1}>Rank 1</MenuItem>
+								<MenuItem value={2}>Rank 2</MenuItem>
+								<MenuItem value={3}>Rank 3</MenuItem>
+								</Select>
+							</FormControl>
+						</div>
+					</Grid>
+					
+				</Grid>
+			</Grid>
+			<Grid item xs={12} md={3}>
+				<SplitButton/>
+				
+			</Grid>
+		</Grid>
 	</>;
 }
 
